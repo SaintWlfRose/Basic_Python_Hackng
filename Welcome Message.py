c3b643580fcb9f2b3cdfbb8aa51be50d4d873086
Joshua Mezieres <97374571+SaintWlfRose@users.noreply.github.com>
2022-01-13 21:38:14 +0800

    Create Welcome Message.py

---
+++ Welcome Message.py
@@ -0,0 +1,7 @@
+print("Welcome everyone") 
+"""This is just a basic project that will consist of different projects in Python. 
+There might be diagrams to show my logic and approach to show how i dealt with problems and issues that i encountered. 
+
+Thank you. 
+From SaintWlfRose 
+"""
